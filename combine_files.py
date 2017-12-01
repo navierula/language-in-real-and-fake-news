f63971ade20ca3517b3d549204bace8345c711db
Navraj Narula <navrajnarula@gmail.com>
2017-11-30 22:39:25 -0500

    combine files

---
+++ combine_files.py
@@ -0,0 +1,27 @@
+import glob2
+
+#######################################################################
+
+# find all file names with a .txt extension
+filenames = glob2.glob('data/political_news/fake_headlines/*.txt') 
+
+# concatenate all individual files into one file
+with open("fake_headlines.txt", "w", encoding="ISO-8859-1") as f:
+    for file in filenames:
+        with open(file, encoding="ISO-8859-1") as infile:
+            # append 'fake' parameter at end of each line
+            f.write(infile.read()+","+"fake"+"\n")
+
+########################################################################
+
+# find all file names with a .txt extension
+filenames = glob2.glob('data/political_news/real_headlines/*.txt') 
+
+# concatenate all individual files into one file
+with open("real_headlines.txt", "w", encoding="ISO-8859-1") as f:
+    for file in filenames:
+        with open(file, encoding="ISO-8859-1") as infile:
+            # append 'fake' parameter at end of each line
+            f.write(infile.read()+","+"real"+"\n")
+
+########################################################################
